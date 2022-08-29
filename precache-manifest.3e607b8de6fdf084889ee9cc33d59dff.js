278a3d9a98ad370d7083731902d28e58dfbf01af
Jonas Holcner <jojkos@gmail.com>
2022-08-29 21:19:51 +0200

    Updates

---
+++ precache-manifest.3e607b8de6fdf084889ee9cc33d59dff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb9f4c23cd2ace5291b7b6b89b295cae",
+    "url": "/testReactReduxApp/index.html"
+  },
+  {
+    "revision": "5ea65bff3929880c7402",
+    "url": "/testReactReduxApp/static/css/main.8fbed401.chunk.css"
+  },
+  {
+    "revision": "868ed10bee0c05504bb9",
+    "url": "/testReactReduxApp/static/js/2.1fec9f88.chunk.js"
+  },
+  {
+    "revision": "5ea65bff3929880c7402",
+    "url": "/testReactReduxApp/static/js/main.3d8de6f4.chunk.js"
+  },
+  {
+    "revision": "f27f009131545f69d008",
+    "url": "/testReactReduxApp/static/js/runtime~main.3a3910ef.js"
+  }
+]);
